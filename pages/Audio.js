e7e147b71956569cae13301c71cad2aff03cbf55
Hillel-Nagid <hillel123123@gmail.com>
2020-06-01 19:26:20 +0300

    Trying callback approach

--- pages/Audio.js
+++ pages/Audio.js
@@ -7,7 +7,7 @@ class Audio extends Component {
 		this.state = { player: '' };
 	}
 	playHandler = () => {
-		if (this.state.player) {
+		if (typeof this.state.player == 'function') {
 			console.log(this.state.player);
 			this.state.player.stop();
 		}
@@ -21,19 +21,21 @@ class Audio extends Component {
 		conductor.setTempo(sectionsProps[0][1]);
 		let piano = conductor.createInstrument('sine');
 		piano.note('quarter', 'G3');
-		this.setState({ player: conductor.finish() });
-		console.log(this.state.player);
-		this.state.player.loop(true);
-		rhythmTimer(sectionsProps[0][0]);
+		this.setState({ player: conductor.finish() }, () => {
+			console.log(this.state.player);
+			this.state.player.loop(true);
+			rhythmTimer(sectionsProps[0][0]);
+		});
+
 		function rhythmTimer(time) {
-			this.state.player.play();
+			Audio.state.player.play();
 			sectionsProps.shift();
 			setTimeout(() => {
-				this.state.player.stop();
+				Audio.state.player.stop();
 				if (sectionsProps.length != 0) {
 					conductor.setTempo(sectionsProps[0][1]);
-					this.setState({ player: conductor.finish() });
-					this.state.player.loop(true);
+					Audio.setState({ player: conductor.finish() });
+					Audio.state.player.loop(true);
 					rhythmTimer(sectionsProps[0][0]);
 				}
 			}, time);
